58333a9145f285d043d159d0d742c6bb31544d47
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-09-11 19:23:32 +0200

    Like in process

--- src/components/events/event-item/EventItem.js
+++ src/components/events/event-item/EventItem.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import moment from "moment";
+import "./EventItem.css";
 
-function EventItem({ title, id }) {
+function EventItem({ title, date, poster, onDeletedEvent, onLikedEvent, id, className, liked }) {
+
+  const handleDeleteEvent = () => onDeletedEvent(id)
+
+  const handleLikeEvent = () => onLikedEvent(id)
+
   return (
-    <li>{title}</li>
-  )
+        <div className={`card ${className}`}>
+          <img src={poster} className="card-img" alt={title} />
+          <div className="card-img-overlay rounded">
+          <span className="date badge text-bg-light">{moment(date).format("D MMM")}</span>
+          <span className="delete-button"><i className="fa fa-times text-danger" role="button" onClick={handleDeleteEvent}/> </span>
+          <span className="like-button"><i className={`fa fa-heart like ${liked ? 'text-danger' : 'text-light'}`} role="button" onClick={handleLikeEvent}/></span>
+          </div>
+          <h6 className="text-center">{title}</h6>
+        </div>
+  );
+}
+
+EventItem.defaultProps = {
+  onDeletedEvent: () => {},
+  onLikedEvent: () => {},
+  liked: false
 }
 
 export default EventItem;
--- src/components/events/event-list/EventList.js
+++ src/components/events/event-list/EventList.js
@@ -6,7 +6,7 @@ import eventData from "../../../data/event";
 function EventList() {
   const [events, setEvents] = useState(eventData);
 
-  
+
 
   const handleCreatedEvent = (event) => {
     setEvents((events) => {
@@ -14,14 +14,27 @@ function EventList() {
     });
   };
 
+  const handleDeleteEvent = (id) => {
+    setEvents((events) => [...events].filter((event) => event.id !== id))
+  }
+
+  const handleLikeEvent = (id) => {
+    setEvents((events) => {
+      return [...events]
+    })
+  }
+
   return (
     <>
       <EventForm onCreatedEvent={handleCreatedEvent} />
-      <ul className="list-group">
+      <hr />
+      <div className="container">
+      <div className="d-flex justify-content-between">
         {events.map((event) => (
-          <EventItem key={event.id} {...event} />
+          <EventItem key={event.id} {...event} onDeletedEvent={handleDeleteEvent} onLikedEvent={handleLikeEvent} className="mx-2"/>
         ))}
-      </ul>
+         </div>
+      </div>
     </>
   );
 }
