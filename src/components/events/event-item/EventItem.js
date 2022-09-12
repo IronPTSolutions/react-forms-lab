0aa56ae6d89627ed8ba2cf5afa5ffdb583a1a74a
Pablo <olbapdow@hotmail.com>
2022-09-12 22:15:46 +0200

    iteration 4 done

--- src/components/events/event-item/EventItem.js
+++ src/components/events/event-item/EventItem.js
@@ -9,15 +9,17 @@ function EventItem({ title, date, poster, onDeletedEvent, onLikedEvent, id, clas
   const handleLikeEvent = () => onLikedEvent(id)
 
   return (
-    <div className={`card ${className} position-relative`}>
-          <img src={poster} className="card-img" alt={title} />
-          <div className="card-img-overlay rounded">
-            <span className="date badge text-bg-light">{moment(date).format("D MMM")}</span>
-            <span className="delete-button"><i className="fa fa-times text-danger" role="button" onClick={handleDeleteEvent}/> </span>
-            <span className="like-button hearth-button"><i className={` fa fa-heart like ${liked ? 'text-danger' : 'text-light'}`} role="button" onClick={handleLikeEvent}/></span>
-          </div>
-          <h6 className="text-center">{title}</h6>
+    <div className="">
+      <div className={`card ${className} position-relative`}>
+        <img src={poster} className="card-img" alt={title} />
+        <div className="card-img-overlay rounded">
+          <span className="date badge text-bg-light">{moment(date).format("D MMM")}</span>
+          <span className="delete-button"><i className="fa fa-times text-danger" role="button" onClick={handleDeleteEvent}/> </span>
+          <span className="like-button hearth-button"><i className={` fa fa-heart like ${liked ? 'text-danger' : 'text-light'}`} role="button" onClick={handleLikeEvent}/></span>
         </div>
+      </div>
+        <h5 className="text-center">{title}</h5>
+    </div>
   );
 }
 
